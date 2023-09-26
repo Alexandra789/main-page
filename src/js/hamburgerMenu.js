459cb9fc585870c9e7859e51551b2959f80fb0bc
Alexandra1234 <alexandra.zakovinko>
2023-09-26 16:14:18 +0300

    add hamburger menu

---
+++ src/js/hamburgerMenu.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger__btn");
+const navMenu = document.querySelector(".hamburger__menu");
+const body = document.querySelector("body");
+const menuLinks = document.querySelectorAll(".hamburger__menu .nav__menu__item");
+const btns = document.querySelectorAll('.hamburger .buttons-wrapper button');
+const menuItems = [...menuLinks, ...btns];
+
+hamburger.addEventListener("click", toggleMobileMenu);
+
+menuItems.forEach(menuItem => {
+	menuItem.addEventListener("click", toggleMobileMenu)
+})
+
+function toggleMobileMenu() {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+	hamburger.classList.contains("active") ? body.style.overflow = 'hidden' : body.style.overflow = 'auto';
+}
