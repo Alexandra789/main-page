9d7681ad3d4933b25ed25f29668bf22ce374931f
Alexandra1234 <alexandra.zakovinko>
2023-09-27 15:36:13 +0300

    formatting

--- src/js/language.js
+++ src/js/language.js
@@ -1,4 +1,4 @@
 const languageBtn = document.querySelector('.language-btn');
-languageBtn.addEventListener('click',(e)=>{
-	e.currentTarget.innerText =  e.currentTarget.innerText === 'EN' ? 'RU' : 'EN';
+languageBtn.addEventListener('click', (e) => {
+	e.currentTarget.innerText = e.currentTarget.innerText === 'EN' ? 'RU' : 'EN';
 })
