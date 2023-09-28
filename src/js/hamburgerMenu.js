4be95d7c02fad6dd9598a8edfe6db51e9b8f367c
Alexandra1234 <alexandra.zakovinko>
2023-09-28 22:57:14 +0300

    fix

--- src/js/hamburgerMenu.js
+++ src/js/hamburgerMenu.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector(".hamburger__btn");
 const navMenu = document.querySelector(".hamburger__menu");
 const body = document.querySelector("body");
 const menuLinks = document.querySelectorAll(".hamburger__menu .nav__menu__item");
-const btns = document.querySelectorAll('.hamburger .buttons-wrapper button');
+const btns = document.querySelectorAll('.hamburger .buttons-wrapper .button');
 const menuItems = [...menuLinks, ...btns];
 
 hamburger.addEventListener("click", toggleMobileMenu);
